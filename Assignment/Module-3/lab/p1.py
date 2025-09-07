f83e07e991353c9918b7c2514c33234d63f8435e
rutvi19 <rutvimandaliya19@gmail.com>
2025-09-07 16:27:21 +0530

    Update p1.py

--- Assignment/Module-3/lab/p1.py
+++ Assignment/Module-3/lab/p1.py
@@ -1,5 +1,5 @@
-player_name = "Bhatt Ji"
-player_age = 28
-fav_language = "Go"
+player_name = "Rutvi"
+player_age = 20
+fav_language = "C"
 
 print(f"My name is {player_name}, I am {player_age} years old and I love {fav_language}.")
