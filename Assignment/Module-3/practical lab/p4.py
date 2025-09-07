82288a249089c11948dcb4823473742214902ac9
rutvi19 <rutvimandaliya19@gmail.com>
2025-09-07 16:32:48 +0530

    Update p4.py

--- Assignment/Module-3/practical lab/p4.py
+++ Assignment/Module-3/practical lab/p4.py
@@ -1,6 +1,6 @@
 # Using 'with' ensures the file is closed automatically
 with open("myfile.txt", "w") as my_file:
-    message = "Hello Bhattji! This message is being saved into the file."
+    message = "Hello Rutvii! This message is being saved into the file."
     print(message, file=my_file)  # print() can directly write to a file
 
 print("Your custom message has been written to myfile.txt")
